33b5ba42bbc6a072b35f1ab3d9f0fd53b5f37443
david-morales-05 <esaueme05@gmail.com>
2026-06-23 14:36:54 -0600

    fix: scrollbar feature has been deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Toaster } from "sonner";
+
 function App() {
   return (
     <>
       <h1 className="font-bold text-6xl ">TaskForge V2</h1>
+
+      <Toaster theme="dark" position="top-right" closeButton />
     </>
   );
 }
